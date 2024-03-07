d54d254e3619233ea11e69ed74fde8084d7c13f8
SuhasRao <suhasrao.thavare13@gmail.com>
2024-03-07 16:24:44 +0530

    feat: edit todo

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ todos=[{"task": "simpletodo","done":False}]
 @app.route('/')
 def index():
     return render_template('index.html',todos=todos)
+
+@app.route("/add",methods=["POST"])
+def add():
+    todo=request.form['todo']
+    todos.append({"task":todo,"done":False})
+    return redirect(url_for("index"))
