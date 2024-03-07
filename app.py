caa46b2c84f3f1bab487029842c9eb5646d6a01b
vineethpawar <vineethpawar99@gmail.com>
2024-03-07 16:32:14 +0530

    feat: add delete todo

--- app.py
+++ app.py
@@ -22,3 +22,16 @@ def edit(index):
         return redirect(url_for("index"))
     else:
         return render_template('edit.html', todo=todo, index=index)
+
+@app.route("/check/<int:index>")
+def check(index):
+    todos[index]['done']=not todos[index]['done']
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    del todos[index]
+    return redirect(url_for("index"))
+
+if __name__=='__main__':
+    app.run(debug=True)
